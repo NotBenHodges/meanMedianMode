5a94ef35bef17914697e80b2e0444f1af204aa22
Ben Hodges <ben.hodges@centre.edu>
2019-02-20 20:18:03 -0500

    Added better explanation

--- reduce.js
+++
@@ -1,8 +0,0 @@
-var numbers = [11, 4, 41, 50, 7, 19, 8, 18, 2, 6, 20, 43, 2, 23, 12, 24, 31, 29];
-
-function getSum(total, num) {
-  return total + num;
-}
-function myFunction(item) {
-  document.getElementById("demo").innerHTML = numbers.reduce(getSum);
-}
