4c00e4e88a24c128d7c24251df2474c458a2dfa0
Ben Hodges <ben.hodges@centre.edu>
2019-02-20 20:03:49 -0500

    Added javascript

--- reduce.js
+++ reduce.js
@@ -1,5 +1,8 @@
-var reduction = function(){
-  var num = [11, 4, 41, 50, 7,
-     19, 8, 18, 2, 6, 20, 43, 2, 23, 12, 24, 31, 29];
-     num.reduce
+var numbers = [11, 4, 41, 50, 7, 19, 8, 18, 2, 6, 20, 43, 2, 23, 12, 24, 31, 29];
+
+function getSum(total, num) {
+  return total + num;
+}
+function myFunction(item) {
+  document.getElementById("demo").innerHTML = numbers.reduce(getSum);
 }
